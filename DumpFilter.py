bc197c977a2e9bedcf1615e4d252f63f28d394ff
0nse <0nse@users.noreply.github.com>
2015-08-04 12:05:07 +0200

    - Do not duplicate page generator as it will lead to another memory leak.

--- DumpFilter.py
+++ DumpFilter.py
@@ -21,8 +21,6 @@ from codecs import encode, decode
 
 from sys import argv
 
-import itertools
-
 def generateOutputFile(fileName, fileSuffix):
     """ Generate an output file name by replacing fileName's extension with
     'xml' or appending '.xml' if no known was found. The fileSuffix is
@@ -52,8 +50,6 @@ def writePage(pageObj, outputFile):
 
     print("[I] Writing revisions of page %s to disk." % pageObj.title)
 
-    pageObj, pageObjCopy = itertools.tee(pageObj)
-    assert areRevisionsSorted(pageObjCopy), '[E] Revisions of "%s" were not sorted ascendingly.' % pageObj.title
     for revisionObj in pageObj:
         revision = ET.Element('revision')
         createSubElement(revision, 'comment', revisionObj)
