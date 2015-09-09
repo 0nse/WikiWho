7773f7c7707611fe3c50514435d445daa28b3891
0nse <0nse@users.noreply.github.com>
2015-09-09 11:03:27 +0200

    + Extend GLMTKPreprocessor to also process individual strings.

--- scripts/GLMTKPreprocessor.py
+++ scripts/GLMTKPreprocessor.py
@@ -5,12 +5,18 @@
 # It was then modified to accept a file input parameter and to use a dedicated
 # method for word extraction from the wordLists.
 
-def split(inputFile):
+def split(textLines):
+  # If a string was passed instead of a file, wrap it inside a list so that
+  # iterating over it will return just this string instead of the individiual
+  # characters:
+  if type(textLines) == str:
+    textLines = [textLines]
+
   with open('ngram-1', 'w') as funi, \
        open('ngram-2', 'w') as fbi,  \
        open('ngram-3', 'w') as ftri, \
        open('ngram-4', 'w') as ffour:
-    for line in inputFile:
+    for line in textLines:
         wordList = line.rstrip('\n').split(' ')
         index = 1
 
@@ -46,7 +52,14 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Prepare a text file to be processed by GLMTK.',
                                    epilog='GLMTKPreprocessor comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions. For more information, see the LICENSE and README.md files this program should have been distributed with.')
 
-  parser.add_argument('inputFile', type=argparse.FileType('r'),
+  parser.add_argument('inputFile', type=argparse.FileType('r'), nargs='?',
                       help='The path to the text file to process.')
+  parser.add_argument('--post', dest="text", type=str, nargs='?',
+                      help='The text to preprocess.')
   args = parser.parse_args()
-  split(args.inputFile)
+  if args.inputFile:
+    split(args.inputFile)
+  elif args.text:
+    split(args.text)
+  else:
+    raise Exception("You must either specify a path to a file or pass a string by using the --post parameter.")
