e48a2f7b1bc26e150b87cab610d9302cb3d03468
0nse <0nse@users.noreply.github.com>
2015-09-27 17:17:29 +0200

    + Add function to randomly sample a notBlocked file balanced according to blocked.

--- scripts/drawRandomElements.sh
+++ scripts/drawRandomElements.sh
@@ -2,12 +2,14 @@
 
 function pick {
   fileName=$1
+  outputFileName=$2
+  amount=$3
 
   length=`wc -l ${fileName} | cut -f1 -d ' '`
   line=0
   usedRandom=(0)
 
-  for ((i=0; i < 15; i++)); do
+  for ((i=0; i < ${amount}; i++)); do
     # duplicate free random line generation:
     while (contains ${line} ${usedRandom[@]}); do
       # generate a new random line number:
@@ -21,20 +23,54 @@ function pick {
       fi
     done
     usedRandom[${i}]=${line}
-    echo -n $line":" >> postsForManualEvaluation_tmp.txt
-    sed -n ${line}' p' ${fileName} >> postsForManualEvaluation_tmp.txt
+    if [[ -z $4 ]]; then
+      echo -n $line":" >> ${outputFileName}
+    fi
+    sed ${line}'q;d' ${fileName} >> ${outputFileName}
   done
 }
 
 function contains {
+  # Usage: contains "value" "array"
+  # Returns true, when the value is in the array.
   local e
   for e in "${@:2}"; do
-    [[ "$e" == "$1" ]] && return 0;
+    if [[ "$e" == "$1" ]]; then
+      return 0;
+    fi
   done
   return 1
 }
 
-pick "run7/blocked.txt"
-pick "run7/notBlocked.txt"
-sort postsForManualEvaluation_tmp.txt > postsForManualEvaluation.txt
-rm postsForManualEvaluation_tmp.txt
+function extractRandomSamples {
+  # Draw 30 samples (15 each) of blocked and not blocked posts. Print them
+  # together with their line number so that it becomes clear where the post
+  # originated from.
+  tmpFileName="postsForManualEvaluation_tmp.txt"
+  pick "run7/blocked.txt" ${tmpFileName} 15
+  pick "run7/notBlocked.txt" ${tmpFileName} 15
+  sort ${tmpFileName} > postsForManualEvaluation.txt
+  rm ${tmpFileName}
+}
+
+function createRandomNotBlockedFile {
+  # Create a notBlocked file which contains as many entries as blocked.txt.
+  # Thus, the result is a balanced, randomly sampled notBlocked file.
+  length=`wc -l "run7/blocked.txt" | cut -f1 -d ' '`
+  pick "run7/notBlocked.txt" "notBlocked_randomlyBalanced.txt" ${length} "don't print line numbers"
+}
+
+case "$1" in
+  (sample)
+    extractRandomSamples
+    exit 0
+    ;;
+  (balance)
+    createRandomNotBlockedFile
+    exit 0
+    ;;
+  (*)
+    echo "Usage: $0 {sample|balance}"
+    exit 1
+    ;;
+esac
