1ec5dfd40ff39634e2b1eecc6a3c5ade079db147
0nse <0nse@users.noreply.github.com>
2015-10-01 18:45:43 +0200

    + Script to efficiently randomly sample not blocked posts.

---
+++ scripts/balanceNotBlocked.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+import subprocess
+
+with open('run7/notBlocked.txt', 'r') as notBlocked, \
+     open('run7/blocked.txt', 'r') as blocked, \
+     open('notBlocked_randomlyBalanced.txt', 'a') as output:
+  length = sum(1 for line in blocked)
+  lines = notBlocked.readlines() # make sure you have the available memory
+  print('Randomly sampling %i lines' % length)
+  sample = random.sample(lines, length)
+
+  for post in sample:
+    output.write(post)
