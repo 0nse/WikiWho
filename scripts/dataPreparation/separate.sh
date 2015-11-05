41003cc1fd98aeab526199870e976c6c574ab8dd
0nse <0nse@users.noreply.github.com>
2015-11-05 17:12:21 +0100

    / Fix one of those senseless syntax errors that makes me loath bash.

--- scripts/dataPreparation/separate.sh
+++ scripts/dataPreparation/separate.sh
@@ -62,26 +62,26 @@ secondsToBlock=$2
 rm -r ../data/blocked ../data/notBlocked
 
 # blocked:
-if [ -n ${secondsToBlock}]; then
+if [ -n "${secondsToBlock}" ]; then
   awk -F $'\t' '{ seconds=int($6);
                   if (seconds > -1 && seconds < int("'"$secondsToBlock"'")) {
                     gsub(/^ +| +$/, "", $5); # trim string
                     print "<BOP> " $5 " <EOP>"
                   }
-                }' $fileName > ../data/blocked.txt
+                }' ${fileName} > ../data/blocked.txt
 fi
 echo "Wrote blocked to disk, now splitting."
 lines=`splitKFold "blocked" 10`
 echo "Split. Continuing with not blocked."
 
 # not blocked:
-if [ -n ${secondsToBlock}]; then
+if [ -n "${secondsToBlock}" ]; then
   awk -F $'\t' '{ seconds=int($6);
                   if (seconds < 0 || seconds >= int("'"$secondsToBlock"'")) {
                     gsub(/^ +| +$/, "", $5); # trim string
                     print "<BOP> " $5 " <EOP>"
                   }
-                }' $fileName > ../data/notBlocked_full.txt
+                }' ${fileName} > ../data/notBlocked_full.txt
 fi
 # This script will randomly draw posts from notBlocked_full.txt and create
 # notBlocked.txt. It will draw as many posts as there are in blocked.txt.
