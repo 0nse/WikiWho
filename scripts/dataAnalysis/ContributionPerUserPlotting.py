cd6479d8c6914e074e43e8648fb7fb6cd626587d
0nse <0nse@users.noreply.github.com>
2015-11-28 21:19:39 +0100

    / Plot cumulative impact on total contributions by most active users.

--- scripts/dataAnalysis/ContributionPerUserPlotting.py
+++ scripts/dataAnalysis/ContributionPerUserPlotting.py
@@ -9,28 +9,46 @@ def plotContributionFrequencyDistribution(postsFile='../../processed/run9/userSo
   import csv
   from collections import Counter
   from matplotlib import pyplot as plt
+  import numpy as np
 
   with open(postsFile, 'r') as inputFile:
     print('[I] starting to process "%s".' % postsFile)
     blockLogReader = csv.reader(inputFile, delimiter='\t', quoting=csv.QUOTE_NONE)
-    users = []
+    contributionAuthors = []
 
     for [_, _, user, _, _, _] in blockLogReader:
-      users.append(user)
+      contributionAuthors.append(user)
 
   # how often was each user active? [(user : #contributions),]
-  userActivity = Counter(users)
-  # how often did users make n many contributions? [(#contributions : frequency),]
-  contributionsFrequency = Counter(userActivity.values())
-  # convert to list  for zip:
-  contributionsFrequency = list(contributionsFrequency.items())
-  contributionsFrequency.sort(reverse=True)
-  # x = contrib frequency, y number of users that contributed that frequently
-  valuesY, valuesX = zip(*contributionsFrequency)
+  userActivity = Counter(contributionAuthors)
 
-  plt.xscale('log')
-  plt.yscale('log')
-  plt.scatter(range(1, len(valuesY)+1), valuesY)
+  mostActiveUser, mostActiveUserContributions = userActivity.most_common(1)[0]
+  print('[I] %i users made %i contributions with %s being the most active user. He/she contributed %i times.' % (len(userActivity), len(contributionAuthors), mostActiveUser, mostActiveUserContributions))
+
+  activity = list(userActivity.values())
+  # most active user's contribution amount first:
+  activity.sort(reverse=True)
+  # start from 0.0:
+  valuesY = [0]
+  valuesY.extend(activity)
+  valuesY = np.cumsum(valuesY)
+
+  valuesX = range(len(valuesY))
+
+  plt.xlabel('Number of most active editors')
+  plt.ylabel('Number of total contributions')
+
+  plt.scatter(valuesX, valuesY)
+
+  for i in valuesX:
+    y = valuesY[i]
+    if y >= len(contributionAuthors) / 2:
+      plt.annotate('Half of all considered contributions were\nmade by the %i most active users.' % i, xy=(valuesX[i], valuesY[i]), xytext=(200,20),
+                   textcoords='offset points', ha='center', va='bottom',
+                   bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=0.3),
+                   arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',
+                                   color='red'))
+      break
 
   # Start the plot at (0.0) and end it at the highest x-value. These calls must
   # be made AFTER plt.scatter.
